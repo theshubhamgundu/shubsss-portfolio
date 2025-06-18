cbcf45736a8ca4b85aaf49381671939824ac0ef6
theshubhamgundu <shubhamvasantgundu@gmail.com>
2025-06-18 21:56:29 +0530

    Added Projects, Blogs, and Contact sections

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to Shubsss💗's Portfolio</h1>
+      <p>Writer & Developer</p>
+      <p>A digital soul with a heart for words.</p>
+    </div>
+  )
+}
+
+export default App
